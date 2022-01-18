e1a5cf18f39d04fa2d6bf3c8b12b8e337cc5befc
vitshan77 <vithushanarul08@gmail.com>
2022-01-18 09:28:39 +0100

    debut de projet

--- src/Vue/connexion.java
+++ src/Vue/connexion.java
@@ -2,11 +2,17 @@ package Vue;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class connexion {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -39,6 +45,23 @@ public class connexion {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(141, 103, 130, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(141, 178, 130, 26);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(168, 70, 61, 16);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(168, 153, 61, 16);
+		frame.getContentPane().add(lblNewLabel_1);
 	}
-
 }
