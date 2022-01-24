85c4c3e32c585a1a521d89b3a4b836f7cabd2282
MinhNguyenTang <minhnguyyen7@gmail.com>
2022-01-24 11:22:17 +0100

    Ajout d'une classe Test
    Ici, pour les tests que vous souhaitez réaliser

---
+++ src/Manager/Test.java
@@ -0,0 +1,11 @@
+package Manager;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Manager manager = New Manager();
+		manager.ClassName();
+	}
+
+}
