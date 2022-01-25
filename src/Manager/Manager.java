0aad83057646f7c56ed24a0f462a8d609a629dc1
vitshan77 <vithushanarul08@gmail.com>
2022-01-25 10:38:02 +0100

    correction bug

--- src/Manager/Manager.java
+++ src/Manager/Manager.java
@@ -4,40 +4,76 @@
 package Manager;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
 public class Manager {
-
-	public void connexion() {
-
-
-		BDD bdd = new BDD();
-		Connection co_bdd = bdd.connection();
+	private int r1;
+	private ResultSet Nomfilm;
+	private String desc;
+	private ResultSet r2;
+	private ResultSet r3;
+	public int exist;
+	public int admin;
+	
+	public Connection connexionbdd (){
+		Connection cnx = null;
+		String url="jdbc:mysql://localhost/hopital_java?serverTimezone=UTC";
+		String user="root";
+		String password="root";
 		try {
-			java.sql.Statement stm = co_bdd.createStatement();
-			ResultSet resultat = stm.executeQuery("SELECT * FROM utilisateurs WHERE nom = '" + textField.getText() + "' AND prenom = '" + textField_1.getText() +"'");
-			if(resultat.next()){
-
-
-				JOptionPane.showMessageDialog(null, " ! !");
-			}
-			else {
-				JOptionPane.showMessageDialog(null, "Mauvais login ou mot de passe!");					 
-			}
-
-
+			cnx = DriverManager.getConnection(url,user, password);		
+			System.out.println(cnx.isClosed()?"fermée":"ouverte");
 		}
-		catch (SQLException e2) {
-
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
+		catch (SQLException e) {
+			System.out.println("Une erreur est survenue lors de la connexion à la base de données");
+			e.printStackTrace();
 		}
+
+		return cnx;
 	}
 
-}
+	public void connexionuser() {
+		
+		Connection co_bdd = this.connexionbdd();
+		java.sql.Statement stm = null;
+		try {
+			stm = co_bdd.createStatement();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}		
+		String re2 = "SELECT email FROM fidelite WHERE email = '" + co.getEmail() + "'" AND mdp = '" + textField_1.getText() +"'";
+		System.out.println(re2);
+
+		try {
+			r2 = stm.executeQuery(re2);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			if (!r2.next()) { /* Si le ResultSet est vide alors exist prend la valeur de 0, on aurait pu faire un boolean également */
+
+				exist = 0;
+			}
+			else {
+				exist = 1; /* Si le ResultSet n'est pas vide alors exist prend la valeur de 1, on aurait pu faire un boolean également */
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return exist;
+	}
+	}
+
+
 
 
 
