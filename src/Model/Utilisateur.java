eb83dbd1c0bbe6291fe5f589481987e76b78f444
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2022-01-26 18:23:14 +0100

    Correction et code aéré

--- src/Model/Utilisateur.java
+++ src/Model/Utilisateur.java
@@ -13,44 +13,56 @@ public class Utilisateur {
 	public int getId() {
 		return this.id;
 		}
+	
 	public void setId(int id) {
 		this.id = id;
-		
-	} 
+		}
+	
 	public String getNom() {
 		return this.nom;
 		}
+	
 	public void setNom(String nom) {
 		this.nom = nom;
 		}
+	
 	public String getPrenom() {
-		return this.nom;
+		return this.prenom;
 		}
+	
 	public void setPrenom(String prenom) {
 		this.nom = prenom;
 		}
+	
 	public String getMail() {
 		return this.mail;
 		}
+	
 	public void setMail(String mail) {
 		this.mail = mail;
 		}
+	
 	public String getMdp() {
 		return this.mdp;
 		}
+	
 	public void setMdp(String mdp) {
 		this.mdp = mdp;
 		}
+	
 	public String getStatus() {
 		return this.status;
 		}
-	public void setMail(String status) {
+	
+	public void setStatus(String status) {
 		this.status = status;
 		}
-	public String getEtatCompte() {
+	
+	public boolean getEtatCompte() {
 		return this.etatCompte;
 		}
-	public void setEtatCompte(String etatCompte) {
+	
+	public void setEtatCompte(boolean etatCompte) {
 		this.etatCompte = etatCompte;
 		}
 	
