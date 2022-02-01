15d88d2a4d9ac8f4068edd4ba66a90a7fc54d7f9
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2022-02-01 11:24:27 +0100

    Constructeur de classe Utilisateur

--- src/Model/Utilisateur.java
+++ src/Model/Utilisateur.java
@@ -9,7 +9,17 @@ public class Utilisateur {
 	private String status;
 	private Boolean etatCompte;
 	
-	
+	public Utilisateur(int id, String nom, String prenom, String mail, String mdp, String status, Boolean etatCompte) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.mail = mail;
+		this.mdp = mdp;
+		this.status = status;
+		this.etatCompte = etatCompte;
+	}
+
 	public int getId() {
 		return this.id;
 		}
