15978f7f6a3af8c9969c359111049ce945769a06
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2022-02-01 08:35:44 +0100

    bin

--- src/Model/CommandeMedicaments.java
+++ src/Model/CommandeMedicaments.java
@@ -5,25 +5,68 @@ public class CommandeMedicaments {
 	private int idInfirmiere;
 	private String nomMedicament;
 	private int nombre;
-	private text libelle;
+	private String libelle;
 	
-	CommandeStock(int id, int idInfirmiere, String nomMedicament, int nombre, text libelle) {
+
+	public CommandeMedicaments(int id, int idInfirmiere, String nomMedicament, int nombre, String libelle) {
+		super();
 		this.id = id;
 		this.idInfirmiere = idInfirmiere;
 		this.nomMedicament = nomMedicament;
 		this.nombre = nombre;
 		this.libelle = libelle;
 	}
-	
-	public int getId() {return this.id;}
-	public void setId(int id) {this.id = id;}
-	public int idInfirmiere() {return this.idInfirmiere;}
-	public void setIdInfirmiere(int idInfirmiere) {this.idInfirmiere = idInfirmiere;}
-	public text getNomMedicament() {return this.nomMedicament;}
-	public void setNomMedicament(String nomMedicament) {this.nomMedicament = nomMedicament;}
-	public int getNombre() {return this.nombre;}
-	public void setNombre(int nombre) {this.nombre = nombre;}
-	public text getLibelle() {return this.libelle;}
-	public void setLibelle(String libelle) {this.libelle = libelle;}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public int getIdInfirmiere() {
+		return idInfirmiere;
+	}
+
+
+	public void setIdInfirmiere(int idInfirmiere) {
+		this.idInfirmiere = idInfirmiere;
+	}
+
+
+	public String getNomMedicament() {
+		return nomMedicament;
+	}
+
+
+	public void setNomMedicament(String nomMedicament) {
+		this.nomMedicament = nomMedicament;
+	}
+
+
+	public int getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(int nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+
 	
 }
