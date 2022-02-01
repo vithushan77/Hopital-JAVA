a732f1954a03f1eb16c27b10c658a8bdf50e097e
vitshan77 <vithushanarul08@gmail.com>
2022-02-01 08:58:54 +0100

    update admin.java

--- src/Vue/admin.java
+++ src/Vue/admin.java
@@ -42,38 +42,36 @@ public class admin {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(0, 191, 255));
-		frame.setBounds(100, 100, 900, 500);
+		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("RDV");
-		btnNewButton.setBackground(new Color(0, 191, 255));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setIcon(new ImageIcon("../Hopital-Java/src/img/rdv.jpeg"));
-		btnNewButton.setBounds(24, 22, 326, 211);
+		btnNewButton.setBounds(106, 27, 205, 40);
 		frame.getContentPane().add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("Hopitalisation");
-		btnNewButton_1.setBounds(524, 22, 326, 211);
+		JButton btnNewButton_1 = new JButton("Ajout utilisateur");
+		btnNewButton_1.setBounds(106, 94, 205, 40);
 		frame.getContentPane().add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("Creé dosier patient");
+		JButton btnNewButton_2 = new JButton("Hopitalisation");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ajoututilisateur ajtu = new ajoututilisateur();
 				ajtu.run();
 			}
 		});
-		btnNewButton_2.setBounds(24, 255, 326, 211);
+		btnNewButton_2.setBounds(106, 153,  205, 40);
 		frame.getContentPane().add(btnNewButton_2);
 		
-		JButton btnNewButton_3 = new JButton("voir des médecins");
-		btnNewButton_3.setBounds(524, 255, 326, 211);
+		JButton btnNewButton_3 = new JButton("Voit de medecin");
+		btnNewButton_3.setBounds(106, 208, 205, 40);
 		frame.getContentPane().add(btnNewButton_3);
+		
+		
 	}
-
-
 }
