bf257f633079923753a85c5bf8846721d1628450
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2022-02-01 08:28:00 +0100

    Test Hash Mdp

--- src/Manager/Test.java
+++ src/Manager/Test.java
@@ -1,11 +1,17 @@
 package Manager;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Manager manager = New Manager();
-		manager.ClassName();
+		String password = "Espellianus";
+		String securePassword = BCrypt.hashpw(password, BCrypt.gensalt(10));
+		System.out.println(securePassword);
+		
+		boolean matched = BCrypt.checkpw(password, securePassword);
+		System.out.println(matched);
 	}
 
 }
